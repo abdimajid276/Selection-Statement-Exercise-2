b7b7704819bf129fa61cc0441ea18ae8d2172cb7
Abdimajid <abdimajid254@gmail.com>
2024-07-17 16:26:18 -0400

    changed geometry subject to lunch

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I dont prefer math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rocks");
+                    break;
+                case "P.E":
+                    Console.WriteLine("P.E is fun");
+                    break;
+                case "History":
+                    Console.WriteLine("History sucks");
+                    break;
+                case "Geometry":
+                    Console.WriteLine("Geometry is fun");
+                    break;
+                default:
+                    Console.WriteLine("i love that too");
+                    break;
+            }
+
         }
     }
 }
